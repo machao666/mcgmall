9390f61d0c264403bf48f865d070bfd713586440
马超 <834997481@qq.com>
2018-08-01 21:13:57 +0800

    新增gmall-list-service工程用于全文检索！！！

---
+++ gmall-list-service/src/main/java/com/atguigu/gmall/gmalllistservice/GmallListServiceApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.gmalllistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallListServiceApplication.class, args);
+	}
+}
---
+++ gmall-list-service/src/test/java/com/atguigu/gmall/gmalllistservice/GmallListServiceApplicationTests.java
@@ -0,0 +1,46 @@
+package com.atguigu.gmall.gmalllistservice;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmallListServiceApplicationTests {
+	@Autowired
+	private JestClient jestClient;
+	@Test
+	public void contextLoads() {
+	}
+	@Test
+	public void testEls() throws IOException {
+		String query = "{\n" +
+				"  \"query\": {\n" +
+				"    \"match\": {\n" +
+				"      \"actorList.name\": \"张译\"\n" +
+				"    }\n" +
+				"  }\n" +
+				"}";
+
+		Search search = new Search.Builder(query).addIndex("movie_chn").addType("movie_type_chn").build();
+
+		SearchResult result = jestClient.execute(search);
+
+		List<SearchResult.Hit<HashMap, Void>> hits = result.getHits(HashMap.class);
+
+		for (SearchResult.Hit<HashMap, Void> hit : hits) {
+			HashMap source = hit.source;
+
+			System.out.println(source.get("name"));
+		}
+	}
+}
